bf1740180cd3736a4355999e6eefffbecd05ec46
이재원 <jwlee@192.168.nate.com>
2019-03-25 00:40:18 +0900

    VerifyPhone Screen

--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Mutation } from 'react-apollo';
-import { RouteComponentProps } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { startPhoneVerification, startPhoneVerificationVariables } from 'src/types/api';
-import PhoneLoginPresenter from './PhoneLoginPresenter';
-import { PHONE_SIGN_IN } from './PhoneQueries.queries';
+import React from "react";
+import { Mutation } from "react-apollo";
+import { RouteComponentProps } from "react-router-dom";
+import { toast } from "react-toastify";
+import {
+  startPhoneVerification,
+  startPhoneVerificationVariables
+} from "../../types/api";
+import PhoneLoginPresenter from "./PhoneLoginPresenter";
+import { PHONE_SIGN_IN } from "./PhoneQueries.queries";
 
 interface IState {
   countryCode: string;
   phoneNumber: string;
 }
 
-class PhoneSignInMutation extends Mutation<startPhoneVerification, startPhoneVerificationVariables> {}
+class PhoneSignInMutation extends Mutation<
+  startPhoneVerification,
+  startPhoneVerificationVariables
+> {}
 
 class PhoneLoginContainer extends React.Component<
   RouteComponentProps<any>,
@@ -23,6 +29,7 @@ class PhoneLoginContainer extends React.Component<
   };
 
   public render() {
+    const { history } = this.props;
     const { countryCode, phoneNumber } = this.state;
     return (
       <PhoneSignInMutation
@@ -39,14 +46,19 @@ class PhoneLoginContainer extends React.Component<
           }
         }}
       >
-        {(mutation, { loading }) => { 
+        {(mutation, { loading }) => {
           const onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
             event.preventDefault();
-            const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(
-              `${countryCode}${phoneNumber}`
-            );
+            const phone = `${countryCode}${phoneNumber}`;
+            const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(phone);
             if (isValid) {
-              mutation();
+              // mutation();
+              history.push({
+                pathname: "/verify-phone",
+                state: {
+                  phone
+                }
+              });
             } else {
               toast.error("Please write a valid phone number");
             }
