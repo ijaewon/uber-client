c0761a32eeb634666d0821345956189e6b428978
ijaewon <ijaewon480@gmail.com>
2019-05-01 00:41:33 +0900

    checking the  git config

--- src/mapHelpers.ts
+++ src/mapHelpers.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import { MAPS_KEY } from './keys';
 
-export const geoCode = async (address: string) => { // 입력한 주소를 좌표로 변환시킴
+export const geoCode = async (address: string) => { // 입력한 주소를 좌표로 변환
   const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${MAPS_KEY}`;
   const { data } = await axios(URL);
   if (!data.error_message) {
